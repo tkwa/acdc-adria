05c05107a5136bf030526a6a9f0c6659031c28b9
tkwa <kwathomas0@gmail.com>
2023-11-15 03:45:20 +0000

    revise tests

--- tests/subnetwork_probing/run_tests.py
+++ tests/subnetwork_probing/run_tests.py
@@ -1,6 +1,7 @@
 # %% 
 import torch
 from tests.subnetwork_probing.test_masked_hookpoint import test_cache_writeable_forward_pass
+from acdc.tracr_task.utils import get_all_tracr_things
 from acdc.docstring.utils import get_all_docstring_things
 from subnetwork_probing.train import MaskedTransformer
 
@@ -8,12 +9,8 @@ from subnetwork_probing.train import MaskedTransformer
 
 test_cache_writeable_forward_pass()
 # %%
-all_task_things = get_all_docstring_things(
-    num_examples=6,
-    seq_len=41,
-    device=torch.device("cpu"),
-    metric_name="kl_div",
-    correct_incorrect_wandb=False,
+all_task_things = get_all_tracr_things(
+    task="reverse", metric_name="l2", num_examples=6, device=torch.device("cpu")
 )
 masked_model = MaskedTransformer(all_task_things.tl_model)
 # %%
@@ -23,4 +20,8 @@ masked_model.do_random_resample_caching(all_task_things.validation_patch_data)
 context_args = dict(ablation='resample', ablation_data=all_task_things.validation_patch_data)
 with masked_model.with_fwd_hooks_and_new_cache(**context_args) as hooked_model:
     out1 = hooked_model(all_task_things.validation_data)
+
+print(masked_model.forward_cache.cache_dict.keys())
+# %%
+masked_model.forward_cache['blocks.0.attn.hook_result']
 # %%
