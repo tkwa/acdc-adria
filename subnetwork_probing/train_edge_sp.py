8bc71cc7234f8f21c73b0ebfea3df2dfece7d005
tkwa <kwathomas0@gmail.com>
2024-01-10 02:59:30 +0000

    move more functions to sp_utils.py

--- subnetwork_probing/train_edge_sp.py
+++ subnetwork_probing/train_edge_sp.py
@@ -10,7 +10,7 @@ import wandb
 from tqdm import tqdm
 from einops import reduce, repeat
 
-from acdc.acdc_utils import filter_nodes, get_edge_stats, get_node_stats, get_present_nodes, reset_network
+from acdc.acdc_utils import filter_nodes, get_present_nodes, reset_network
 from acdc.docstring.utils import AllDataThings, get_all_docstring_things, get_docstring_subgraph_true_edges
 from acdc.tracr_task.utils import get_tracr_proportion_edges, get_tracr_reverse_edges
 from acdc.greaterthan.utils import get_all_greaterthan_things, get_greaterthan_true_edges
@@ -19,40 +19,8 @@ from acdc.ioi.utils import get_all_ioi_things, get_ioi_true_edges
 from acdc.TLACDCCorrespondence import TLACDCCorrespondence
 from acdc.TLACDCEdge import Edge
 from acdc.tracr_task.utils import get_all_tracr_things
-from subnetwork_probing.sp_utils import MaskedTransformer, edge_level_corr
+from subnetwork_probing.sp_utils import MaskedTransformer, edge_level_corr, print_stats, set_ground_truth_edges
 
-
-# %%
-
-def set_ground_truth_edges(canonical_circuit_subgraph: TLACDCCorrespondence, ground_truth_set: set):
-    for (receiver_name, receiver_index, sender_name, sender_index), edge in canonical_circuit_subgraph.all_edges().items():
-        key =(receiver_name, receiver_index.hashable_tuple, sender_name, sender_index.hashable_tuple)
-        edge.present = (key in ground_truth_set)
-
-
-def print_stats(recovered_corr, ground_truth_subgraph, do_print=True):
-    """
-    False positive = present in recovered_corr but not in ground_truth_set
-    False negative = present in ground_truth_set but not in recovered_corr
-    """
-    # diff = set(recovered_corr.all_edges().keys()) - set(ground_truth_subgraph.all_edges().keys())
-    # if diff: print(f"{len(diff)} key mismatches: {diff}")
-    stats = get_node_stats(ground_truth=ground_truth_subgraph, recovered=recovered_corr)
-    node_tpr = stats["true positive"] / (stats["true positive"] + stats["false negative"])
-    node_fpr = stats["false positive"] / (stats["false positive"] + stats["true negative"])
-    if do_print:print(f"Node TPR: {node_tpr:.3f}. Node FPR: {node_fpr:.3f}")
-
-    stats = get_edge_stats(ground_truth=ground_truth_subgraph, recovered=recovered_corr)
-    edge_tpr = stats["true positive"] / (stats["true positive"] + stats["false negative"])
-    edge_fpr = stats["false positive"] / (stats["false positive"] + stats["true negative"])
-    if do_print:print(f"Edge TPR: {edge_tpr:.3f}. Edge FPR: {edge_fpr:.3f}")
-
-    return {
-        "node_tpr": node_tpr,
-        "node_fpr": node_fpr,
-        "edge_tpr": edge_tpr,
-        "edge_fpr": edge_fpr,
-    }
 
 def train_sp(
     args,
